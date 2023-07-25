9213fb9c547e8e5ca4359e3d3cc0e9106decf9e3
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-07-25 11:23:39 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,7 @@
+//get the first child and last child and the all childs of html using javaScript
+document.body.firstChild();
+document.body.lastChild();
+document.body.Child();
+
+// to know more, go through the MDN. https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model --> interface --> node 
+// 
